2dbdd4ef3a69dab1db2e075cfa5888c6583906aa
Pedro Carvalho <pedrocarvalhoresende@gmail.com>
2025-03-24 19:43:43 -0300

    Botão de menu responsivo

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  const menuBtn = document.getElementById('menu-btn');
+  const navBar = document.querySelector('.nav-bar');
+
+  menuBtn.addEventListener('click', () => {
+    navBar.classList.toggle('active');
+  });
+
